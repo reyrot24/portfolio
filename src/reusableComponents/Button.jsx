d14f65d190195b826e728c79ed26cc0f31d3dbe5
reyrot24 <reyrotaru1@gmail.com>
2024-04-26 12:13:23 +0200

    button links work

--- src/reusableComponents/Button.jsx
+++ src/reusableComponents/Button.jsx
@@ -4,31 +4,39 @@ function Button({ title, type }) {
   if (type === "1") {
     return (
       <div className="cursor-pointer  animate-bg  text-black w-full lg:w-56 m-2 text-center button rounded">
-        <button>
-          <a href="#skills">{title}</a>
+        <button className="w-full">
+          <a href="#skills" className="block">
+            {title}
+          </a>
         </button>
       </div>
     );
   } else if (type === "2") {
     return (
-      <div className=" text-buttonText px-3 py-3 rounded">
-        <button className="animate-bg px-3 py-1 rounded block text-white/80">
-          <a href="#contact">{title}</a>
+      <div className="text-buttonText px-3 py-3 rounded">
+        <button className="w-full animate-bg px-3 py-1 rounded block text-white/80">
+          <a href="#contact" className="block">
+            {title}
+          </a>
         </button>
       </div>
     );
   } else if (type === "3") {
     return (
       <div className="cursor-pointer border border-softBrown animate-text w-full lg:w-56 m-2 button text-center hover:border-softerBrown/60 hover:shadow hover:shadow-softBrown duration-300 rounded">
-        <button>
-          <a href="#contact">{title}</a>
+        <button className="w-full">
+          <a href="#contact" className="block">
+            {title}
+          </a>
         </button>
       </div>
     );
   } else {
     return (
       <div className="cursor-pointer border border-softBrown animate-text w-full lg:w-56 m-2 button text-center hover:border-softerBrown/60 hover:shadow hover:shadow-softBrown duration-300 rounded">
-        <button type="submit">{title}</button>
+        <button type="submit" className="w-full">
+          {title}
+        </button>
       </div>
     );
   }
